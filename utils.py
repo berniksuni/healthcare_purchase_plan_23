ad6576ced92378405a2510fa39fb777b663bb693
berniksuni <bernmedper@gmail.com>
2023-11-11 20:36:57 +0100

    la gatita

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
 
 def preprocess_cosumo(data):
     # Add column for total stack bought
@@ -9,10 +11,29 @@ def preprocess_cosumo(data):
     separated_origin = data['ORIGEN'].str.extract(r'(\d+)\D+(\d+)\D+(\d+)')
     data['REGION'], data['HOSPITAL'], data['DEPARTMENT'] = separated_origin[0], separated_origin[1], separated_origin[2]
     # Drop unnecessary columns
-    data.drop(['PRODUCTO', 'NUMERO','ORIGEN','FECHAPEDIDO'], axis = 1, inplace = True)
+    data.drop(['PRODUCTO', 'NUMERO','ORIGEN','CANTIDADCOMPRA','IMPORTELINEA'], axis = 1, inplace = True)
     # One hot encode the columns
-    data_ohe = pd.get_dummies(data, columns=['CODIGO', 'REFERENCIA','TIPOCOMPRA', 'TGL', 'REGION', 'HOSPITAL', 'DEPARTMENT'])
-    data_ohe.iloc[:, 6:] = data_ohe.iloc[:, 6:].astype('int')
+    data_ohe = pd.get_dummies(data, columns=['CODIGO', 'REFERENCIA','TIPOCOMPRA', 'TGL', 'REGION', 'HOSPITAL', 'DEPARTMENT', 'CATEGORY'])
+    data_ohe.iloc[:, 3:] = data_ohe.iloc[:, 3:].astype('int')
     print("asjfkjdsfljds")
     return data_ohe
+
+def residuals_hist(y_true, y_pred):
+    residuals = np.abs(y_true - y_pred)
+    # Create a histogram of residuals
+    plt.hist(residuals, bins=50, edgecolor='black')
+    plt.title('Distribution of Residuals')
+    plt.xlabel('Residuals')
+    plt.ylabel('Frequency')
+    plt.show()
+
+def residuals_scatter(y_true, y_pred):
+    residuals = np.abs(y_true - y_pred)
+    # Alternatively, create a scatter plot
+    plt.scatter(y_pred, residuals, alpha=0.5)
+    plt.axhline(y=0, color='red', linestyle='--')  # Add a horizontal line at y=0 for reference
+    plt.title('Residuals Scatter Plot')
+    plt.xlabel('True Values')
+    plt.ylabel('Residuals')
+    plt.show()
 
